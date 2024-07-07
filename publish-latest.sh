1c4c1f574555fc97faf11508f37a02b6823311fc
Alex Tselegidis <alextselegidis@gmail.com>
2024-07-07 14:15:36 +0200

    Add script for publishing the latest release

---
+++ publish-latest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# Easy!Appointments - Online Appointment Scheduler
+#
+# @package     EasyAppointments
+# @author      A.Tselegidis <alextselegidis@gmail.com>
+# @copyright   Copyright (c) Alex Tselegidis
+# @license     https://opensource.org/licenses/GPL-3.0 - GPLv3
+# @link        https://easyappointments.org
+# -----------------------------------------------------------------------------
+
+##
+# Publish a docker image tag for an Easy!Appointments release.
+#
+# Usage:
+#
+#  ./publish-latest.sh <version>
+#
+# Example:
+#
+#   ./publish-latest.sh 1.5.0
+#
+
+DEFAULT_VERSION=1.5.0
+
+VERSION="${1:-$DEFAULT_VERSION}"
+
+docker tag easyappointments:$VERSION alextselegidis/easyappointments:latest
+
+docker push alextselegidis/easyappointments:latest
