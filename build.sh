909b989398b9f0add70b6c0b0a446ddcb7f74689
Alex Tselegidis <alextselegidis@gmail.com>
2025-02-24 08:47:28 +0100

    Multi-arch build (#8)

--- build.sh
+++ build.sh
@@ -29,5 +29,4 @@ DEFAULT_VERSION=1.5.0
 
 VERSION="${1:-$DEFAULT_VERSION}"
 
-docker build --tag alextselegidis/easyappointments:${VERSION} --build-arg VERSION=${VERSION} .
-#docker build --tag alextselegidis/easyappointments:${VERSION} --build-arg VERSION=${VERSION} --platform linux/amd64,linux/arm64 .
+docker buildx build --platform linux/amd64,linux/arm64 -t alextselegidis/easyappointments:${VERSION} --build-arg VERSION=${VERSION} .
